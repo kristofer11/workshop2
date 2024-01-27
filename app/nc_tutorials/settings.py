1dd7512b1c0391e24d2c4fb8ddfc85f8de247a0f
Kris Hvattum <klhvattum@live.com>
2024-01-27 12:41:10 -0800

    update .env, etc

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-52-15-51-188.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'ec2-52-15-51-188.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+]
 
 
 # Application definition
